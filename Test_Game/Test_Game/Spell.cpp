1a4e99af22180940a55f15d3cdda620073e07168
GothicDread <dreadlock54@hotmail.com>
2019-04-30 00:58:39 +0100

    Added some comments to the code; Added wall of text to Spell.h

--- Test_Game/Test_Game/Spell.cpp
+++ Test_Game/Test_Game/Spell.cpp
@@ -1,7 +1,7 @@
 #include "Spell.h"
 
-Spell::Spell(string n, int dmg, int hv, int d, bool id, bool ih, bool at, bool tt, bool ft, bool it, bool pt, bool hh):
-	name(n), damage(dmg), heal(hv), dot(d), isDamaging(id), isHealing(ih), isArcane(at), isThunder(tt), isFire(ft), isIce(it), isPoison(pt), hasHealOverTime(hh) {}
+Spell::Spell(string n, int dmg, int hv, int d, int h, bool id, bool ih, bool at, bool tt, bool ft, bool it, bool pt, bool nt, bool hh):
+	name(n), damage(dmg), heal(hv), dot(d), hot(h), isDamaging(id), isHealing(ih), isArcane(at), isThunder(tt), isFire(ft), isIce(it), isPoison(pt), isNature(nt), hasHealOverTime(hh) {}
 
 
 Spell::~Spell()
--- Test_Game/Test_Game/spells_include.h
+++ Test_Game/Test_Game/spells_include.h
@@ -1,3 +1,5 @@
+//We'll add the spell header file include to this file to save lines in classes that use spells
+
 #ifndef SPELLS_INCLUDE_H
 #define SPELLS_INCLUDE_H
 
