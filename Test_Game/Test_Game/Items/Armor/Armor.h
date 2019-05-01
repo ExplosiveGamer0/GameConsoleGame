ed660dbbefbce4a3c8befcceedf4f6bf8ed77356
Ben Smallwood <tekit67@gmail.com>
2019-05-01 21:55:47 +0100

    New files

---
+++ Test_Game/Test_Game/Items/Armor/Armor.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include"../Item.h"
+
+class Armor : public Item
+{
+	//Name and stuff
+	const string name;
+	const string description;
+
+	//Buy and sell value
+	const int buyValue;
+	const int sellValue;
+
+	//Defence and stats
+	const int defence;
+	const int accuracy;
+	const int damageBuff;
+	// This will be a percentage buff depending on the weapon or spell used
+	// So if you use this on a chaos spell it could have a damage buff of 15% and then a sword could have a damage buff of 5%
+	const int luck; // Allows the player to have a higher percentage to get drops
+	const int healthBuff; // Buffs health
+	const int staminaBuff; // Buffs stamina
+
+						   /////////////////////////
+						   // GETTERS AND SETTERS //
+						   /////////////////////////
+	inline const string getName() const { return this->name; }
+	inline const int getBuyValue() const { return this->buyValue; }
+	inline const int getSellValue() const { return this->sellValue; }
+	inline const int getDefence() const { return this->defence; }
+	inline const int getAccuracy() const { return this->accuracy; }
+	inline const int getDamageBuff() const { return this->damageBuff; }
+	inline const int getLuck() const { return this->luck; }
+	inline const int getHealthBuff() const { return this->healthBuff; }
+	inline const int getStaminaBuff() const { return this->staminaBuff; }
+
+public:
+
+	Armor();
+
+	Armor(string name, int buyValue, int sellValue, int defence, int accuracy, int damageBuff, int luck, int healthBuff, int staminaBuff);
+
+	~Armor();
+
+};
+
--- Test_Game/Test_Game/Items/Item.h
+++ Test_Game/Test_Game/Items/Item.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include"includes.h"
+#include"../includes.h"
 
 class Item
 {
@@ -12,19 +12,23 @@ class Item
 	const int buyValue;
 	const int sellValue;
 
+	/**
 	//Sets up the damage
 	const int damageMin;
 	const int damageMax;
+	**/
 
 	//Getters and Setters
 	inline const string getName() const { return this->name; }
 	inline const string getDescription() const { return this->description; }
 	inline const int getBuyValue() const { return this->buyValue; }
 	inline const int getSellValue() const { return this->sellValue; }
+	/**
 	inline const int getDamageMin() const { return this->damageMin; }
 	inline const int getDamageMax() const { return this->damageMax; }
 
-	Item(string name, string description, int buyValue, int sellValue, int damageMin, int damageMax);
+	**/
+	Item(string name, string description, int buyValue, int sellValue);
 
 public:
 
